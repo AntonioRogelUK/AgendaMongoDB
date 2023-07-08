4c2c796df1bc3e790ec6a58ddbde67c5b83f51c9
Raymundo Daniel Ramírez Ramos <ray.ramirez@unikino.edu.mx>
2023-07-08 10:51:30 -0700

    CRUD contacto

---
+++ AgendaMongoDB/Contacto.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaMongoDB
+{
+    internal class Contacto
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [BsonElement("address")]
+        public string Address { get; set; } = string.Empty;
+
+        [BsonElement("phone")]
+        public string Phone { get; set; } = string.Empty;
+
+    }
+}
--- AgendaMongoDB/Program.cs
+++ AgendaMongoDB/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AgendaMongoDB;
+using MongoDB.Driver;
+
+MongoClient client = new MongoClient("mongodb://localhost:27017");
+IMongoDatabase database = client.GetDatabase("agenda");
+IMongoCollection<Contacto> collection = database.GetCollection<Contacto>("contactos");
+
+Contacto contacto= new Contacto();
+contacto.Name = "Putter";
+contacto.Address = "9 3/4";
+contacto.Phone= "1234567890";
+
+Insert(contacto);
+
+
+Contacto contacto2 = new Contacto();
+contacto2.Name = "Sasuke";
+contacto2.Address = "Konoha";
+contacto2.Phone= "1234567890";
+
+Insert(contacto2);
+
+Contacto contactoEncontrado = FindByName("Putter");
+Console.WriteLine(contactoEncontrado.Id);
+
+
+foreach(Contacto c in Get())
+{
+    Console.WriteLine(c.Name);
+}
+
+void Insert(Contacto contacto)
+{
+    collection.InsertOne(contacto);
+}
+
+Contacto FindByName(string name)
+{
+    return collection.Find(c => c.Name == name)
+        .FirstOrDefault<Contacto>();
+}
+
+List<Contacto> Get()
+{
+    return collection.Find(c => true).ToList();
+}
